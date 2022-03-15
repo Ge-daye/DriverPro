eba5e692871c975a610d87a064512e39e10589e9
Ge-daye <1815550182@qq.com>
2022-03-15 12:51:11 +0800

    添加knife4j配置

--- src/main/java/vip/george366/driverpro/controller/UserController.java
+++ src/main/java/vip/george366/driverpro/controller/UserController.java
@@ -1,11 +1,17 @@
 package vip.george366.driverpro.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 葛浩东
  */
+@Api(tags = "用户模块")
 @RestController
 public class UserController {
 
@@ -13,5 +19,13 @@ public class UserController {
     public String show(){
         return "Tom 你好";
     }
+
+    @ApiImplicitParam(name = "name",value = "姓名",required = true)
+    @ApiOperation(value = "向客人问好")
+    @GetMapping("/sayHi")
+    public ResponseEntity<String> sayHi(@RequestParam(value = "name")String name){
+        return ResponseEntity.ok("Hi:"+name);
+    }
+
 
 }
